faef61450afc87d96591e7eeb1279765974b09c9
dark <mdomagala@darkabo.eu>
2026-01-08 21:12:48 +0100

    fix steel conversion

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -6,8 +6,8 @@ ServerEvents.recipes(event => {
     event.shaped(Item.of('immersiveengineering:treated_wood_horizontal', 4), ['   ',' AA',' AA'],{ A: 'tfmg:hardened_planks'});
     event.shaped(Item.of('create_new_age:copper_wire', 4), ['   ','  A','  A'],{ A: 'immersiveengineering:wire_copper' });
     event.shaped(Item.of('immersiveengineering:wire_copper', 1), ['   ','  A','  A'],{ A: 'create_new_age:copper_wire' });
-    event.shaped(Item.of('tfmg:steel_ingot', 1), ['   ',' AA',' AA'],{ A: 'immersiveengineering:ingot_steel'});
-    event.shaped(Item.of('immersiveengineering:ingot_steel', 1), ['   ',' AA',' AA'],{ A: 'tfmg:steel_ingot'});
+    event.shaped(Item.of('tfmg:steel_ingot', 4), ['   ',' AA',' AA'],{ A: 'immersiveengineering:ingot_steel'});
+    event.shaped(Item.of('immersiveengineering:ingot_steel', 4), ['   ',' AA',' AA'],{ A: 'tfmg:steel_ingot'});
     event.shapeless(Item.of('tfmg:nickel_sheet', 1), [ 'immersiveengineering:plate_nickel' ] );
     event.shapeless(Item.of('immersiveengineering:plate_nickel', 1), [ 'tfmg:nickel_sheet' ] );
     event.shapeless(Item.of('tfmg:lead_sheet', 1), [ 'immersiveengineering:plate_lead' ] );
