bae5873ad9a2848f3db970ab6e158567fa2115c2
dark <mdomagala@darkabo.eu>
2026-01-08 14:33:12 +0100

    fix recipes and configure one probe

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,3 +1,42 @@
 // Visit the wiki for more info - https://kubejs.com/
-console.info('Hello, World! (Loaded server example script)')
+console.info('Loading Custom Recipes')
 
+ServerEvents.recipes(event => {
+    event.shaped(Item.of('tfmg:hardened_planks', 4), 
+        [
+        '   ',
+        ' AA',
+        ' AA'
+        ],
+        { A: 'immersiveengineering:treated_wood_horizontal'}
+    )
+    event.shaped(Item.of('immersiveengineering:treated_wood_horizontal', 4), 
+        [
+        '   ',
+        ' AA',
+        ' AA'
+        ],
+        { A: 'tfmg:hardened_planks'}
+    )
+    event.shaped(Item.of('create_new_age:copper_wire', 4), 
+        [
+        '   ',
+        '  A',
+        '  A'
+        ],
+        { A: 'immersiveengineering:wire_copper' }
+    )
+    event.shaped(Item.of('immersiveengineering:wire_copper', 1), 
+        [
+        '   ',
+        '  A',
+        '  A'
+        ],
+        { A: 'create_new_age:copper_wire' }
+    )
+
+
+
+
+    console.log('Recipes Loaded')
+})
